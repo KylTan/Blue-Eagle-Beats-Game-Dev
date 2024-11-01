844d6ac9f313a441e230ba9d6fae9ac577f23891
KylTan <ezekiel.tan@obf.ateneo.edu>
2024-11-01 17:22:24 +0800

    partial additions to adapting the snare gamemode

---
+++ Objects/Snare_Note.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+@export var line: int
+
+var leftNote = preload("res://Assets/Snare Inner Left.png")
+var rightNote = preload("res://Assets/Snare Inner Right.png")
+
+var notePosition
+var is_colliding = false
+var collected = false
+var receiver 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_Position()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	collect()
+
+func set_Material():
+	match line:
+		1:
+			$Sprite2D.texture = leftNote
+		2:
+			$Sprite2D.texture = rightNote
+func set_Position():
+	self.position = Vector2 (0, -notePosition)
+
+func collect():
+	if !collected:
+		if is_colliding and receiver.is_Hit: #if its colliding and can access the receiver
+			collected = true
+			receiver.is_Hit = false
+			hide()
+			
+			
+func _on_area_2d_area_entered(area):
+	if area.is_in_group("receiver"):
+		is_colliding = true
+		receiver = area.get_parent()
+
+func _on_area_2d_area_exited(area):
+	if area.is_in_group("receiver"):
+		is_colliding = false
