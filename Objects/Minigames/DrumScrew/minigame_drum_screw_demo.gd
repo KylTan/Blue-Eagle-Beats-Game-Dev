95942eb506be17b1586ed10bdf22491a2e7397a7
KylTan <ezekiel.tan@obf.ateneo.edu>
2025-02-19 18:54:46 +0800

    sequencing level 2 snare

--- Objects/Minigames/DrumScrew/minigame_drum_screw_demo.gd
+++ Objects/Minigames/DrumScrew/minigame_drum_screw_demo.gd
@@ -9,18 +9,38 @@ var cursor = preload("res://Assets/DrumScrewMinigame/MG_Screw_RatchetKey.png")
 func _ready() -> void:
 	Input.set_custom_mouse_cursor(cursor, Input.CURSOR_ARROW, Vector2(75,90))
 	screwTotal = 4
-	
-	match nextDialogueIndex:
-		1: #first training arc
-			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_post_training.tscn"
-		2:
-			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_post_maintenance.tscn"
-		3:
-			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_1sthalf_basketball_game.tscn"
-		4:
-			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_2ndhalf_basketball_game.tscn"
-		5:
-			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_post_basketball_game.tscn"
+
+	if nextDialogueIndex: # CHECK FOR SCREW GAME TO DIFF BETWEEN BASS TICKS AND SNARE TICKS
+		if Dialogic.VAR.Dialogue_name > 0:
+			nextDialogueIndex = Dialogic.VAR.Dialogue_name #number corresponds to a scene hopefully
+			match nextDialogueIndex:
+				# Bass
+				1: #first training arc
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_post_training.tscn"
+				2:
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_post_maintenance.tscn"
+				3:
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_1sthalf_basketball_game.tscn"
+				4:
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_2ndhalf_basketball_game.tscn"
+				5:
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_post_basketball_game.tscn"
+			
+		elif Dialogic.VAR.Dialogue_name_snare > 0:
+			nextDialogueIndex = Dialogic.VAR.Dialogue_name_snare #number corresponds to a scene hopefully
+			match nextDialogueIndex:
+				1: #first training arc
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_post_training.tscn" # 2nd dialog
+				2: #first training arc
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_post_maintanance_1.tscn"
+				#3: # first bball - pre bball - omitted since its a mission start thing
+					#nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_pre_bball.tscn"
+				3: # first bball - 1st half
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_1sthalf_bball.tscn"
+				4: # first bball - 2nd half
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_2ndhalf_bball.tscn"
+				5: # first bball - post bball
+					nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_post_bball.tscn"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Objects/main_game_scene_snare.gd
+++ Objects/main_game_scene_snare.gd
@@ -20,14 +20,29 @@ func _ready():
 		1: #first training
 			mapfile = "res://Assets/Audio/Otso-Cheer-Snare.mboy"
 			audiofile = "res://Assets/Audio/Otso-Cheer.mp3"
+		2: #first bball - endurance
+			mapfile = "res://Assets/Audio/1 Min Endurance - First Bball Game Snare.mboy"
+			audiofile = "res://Assets/Audio/Endurance String 1 minute.mp3"
+		3: #first bball - halftime cheer
+			mapfile = "res://Assets/Audio/3-Fights-Cheer-Snare.mboy"
+			audiofile = "res://Assets/Audio/3Fights.mp3"
+		4: # Finals - Pre game
+			mapfile = "res://Assets/Audio/Manson-Snare.mboy"
+			audiofile = "res://Assets/Audio/Manson.mp3"
 	
 	match nextDialogueIndex:
 		1: #first training arc
 			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_post_training.tscn" # 2nd dialog
 		2: #first training arc
 			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_post_maintanance_1.tscn"
-		3: # LVL 2 - pre bball
-			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_pre_bball.tscn"
+		#3: # first bball - pre bball - omitted since its a mission start thing
+			#nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_pre_bball.tscn"
+		3: # first bball - 1st half
+			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_1sthalf_bball.tscn"
+		4: # first bball - 2nd half
+			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_2ndhalf_bball.tscn"
+		5: # first bball - post bball
+			nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_snare_post_bball.tscn"
 			
 	var instance = snareGame.instantiate()
 	instance.audiofile = audiofile
