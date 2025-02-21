b5c51ac4b19542d2112ffc984cccb2f8a902c12e
KylTan <ezekiel.tan@obf.ateneo.edu>
2025-02-21 13:24:15 +0800

    finished GOAT Snare

--- Objects/main_game_scene_snare.gd
+++ Objects/main_game_scene_snare.gd
@@ -1,7 +1,7 @@
 extends Node2D
 @onready var snareGame = preload("res://Objects/Snare Game.tscn")
-var mapfile = "res://Assets/Audio/Manson-Snare.mboy"
-var audiofile = "res://Assets/Audio/Manson.mp3"
+var mapfile = "res://Assets/Audio/Go Ateneo-Snare 2.0.mboy"
+var audiofile = "res://Assets/Audio/Go Ateneo (BrassAlt+Banda).mp3"
 
 var nextDialogueScene = "res://Objects/Dialogue_Scenes/timeline_pre_training.tscn"
 var nextCheerIndex = 1 #intercepts what the next cheer index is
