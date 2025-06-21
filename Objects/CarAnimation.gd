2a537a651b2cd7e1a40770dbdda146cbc2324b53
KylTan <ezekiel.tan@obf.ateneo.edu>
2025-06-22 00:30:22 +0800

    Add Car
    Car thats in the training BG

---
+++ Objects/CarAnimation.gd
@@ -0,0 +1,13 @@
+extends AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Timer.start(randi_range(3,30)) 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_timer_timeout():
+	play("Car driving")
