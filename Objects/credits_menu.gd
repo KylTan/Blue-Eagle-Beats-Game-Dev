9514813df57db4f59e8490e4c43a32fde2e7b074
KylTan <ezekiel.tan@obf.ateneo.edu>
2025-06-26 21:07:06 +0800

    UI Improvements
    - new button design
    - new credits

--- Objects/credits_menu.gd
+++ Objects/credits_menu.gd
@@ -11,7 +11,12 @@ func _ready():
 func _process(delta):
 	if $SubViewportContainer/SubViewport.anim_fin1 == 1:
 		$Credits.visible = true
+		$AnimationPlayer.play("hover")
 
 
 func _on_back_button_pressed():
 	GlobalSceneManager._changeScene_MainMenu()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	$AnimationPlayer.play("hover")
