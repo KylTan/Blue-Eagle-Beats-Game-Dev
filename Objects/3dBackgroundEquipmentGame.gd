85ebd10f8449539c1bb7e45c70fdd479d3894c29
KylTan <ezekiel.tan@obf.ateneo.edu>
2025-07-03 13:29:57 +0800

    more sound added
    - added to equipment game
    - tweaked sound levels of main game

--- Objects/3dBackgroundEquipmentGame.gd
+++ Objects/3dBackgroundEquipmentGame.gd
@@ -4,11 +4,16 @@ var animFin = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("DoorOpening")
-
-
+	$BusArriving.play()
+	$BGSound.play()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_animation_player_animation_finished(anim_name):
 	animFin = 1
+
+
+func _on_bg_sound_finished():
+	$BGSound.play()
